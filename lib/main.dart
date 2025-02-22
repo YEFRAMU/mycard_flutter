2a2375cd4b371f1392f59d14ee3b0bf73c8e0033
YEFRAMU <vinceumali81@gmail.com>
2025-02-22 23:07:32 +0800

    Practice Layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,47 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: f alse,
+      debugShowCheckedModeBanner: false,
+      // debugShowMaterialGrid: true,
       home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          //SafeArea widget used for avoiding the status bar or like the notch of the phone
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.red,
+                child: Text("Container 1"),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.yellow,
+                    child: Text("Container 2"),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.green,
+                    child: Text("Container 3"),
+                  ),
+                ],
+              ),
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.blue,
+                child: Text("Container 4"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
